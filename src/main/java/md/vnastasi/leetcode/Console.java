745988a79f737a04afa0ed8eabd09602659f41d6
Valentin Nastasi <pragmatiku@protonmail.com>
2022-10-15 15:44:51 +0200

    1. Two sum

---
+++ src/main/java/md/vnastasi/leetcode/Console.java
@@ -0,0 +1,25 @@
+package md.vnastasi.leetcode;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public final class Console {
+
+    private static final Scanner SCANNER = new Scanner(System.in);
+
+    private Console() {}
+
+    public static int readInt(String message) {
+        System.out.print(message + ": ");
+        return Integer.parseInt(SCANNER.nextLine());
+    }
+
+    public static int[] readIntArray(String message) {
+        System.out.print(message + " (values must be separated by space): ");
+        return Arrays.stream(SCANNER.nextLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+    }
+
+    public static void write(String format, Object... args) {
+        System.out.printf(format, args).println();
+    }
+}
---
+++ src/main/java/md/vnastasi/leetcode/TwoSum.java
@@ -0,0 +1,45 @@
+package md.vnastasi.leetcode;
+
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+/*
+ * Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+ * You may assume that each input would have exactly one solution, and you may not use the same element twice.
+ * You can return the answer in any order.
+ *
+ * Example 1:
+ * Input: nums = [2,7,11,15], target = 9
+ * Output: [0,1]
+ * Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+ *
+ * Example 2:
+ * Input: nums = [3,2,4], target = 6
+ * Output: [1,2]
+ *
+ * Example 3:
+ * Input: nums = [3,3], target = 6
+ * Output: [0,1]
+ */
+public class TwoSum {
+
+    public static void run() {
+        var nums = Console.readIntArray("Enter nums array");
+        var target = Console.readInt("Enter target value");
+
+        for (var i = 0; i < nums.length; i++) {
+            for (var j = i + 1; j < nums.length; j++) {
+                if (nums[i] + nums[j] == target) {
+                    Console.write("Found solution: [%d, %d]", i, j);
+                    return;
+                }
+            }
+        }
+
+        Console.write("No solution found");
+    }
+
+    public static void main(String[] args) {
+        run();
+    }
+}
